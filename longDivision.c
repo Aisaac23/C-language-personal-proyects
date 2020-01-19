28d0e77a93ee256c749072417945412caa24352d
Aisaac23 <isaac.huertah22@gmail.com>
2020-01-19 16:03:14 -0600

    major changes: a lot less space required in memory.

--- longDivision.c
+++ longDivision.c
@@ -4,91 +4,98 @@
 #include <stdbool.h>
 /*The program receives as arguments, two unsigned integers, first the dividend and then the divisor. The dividend should alwyas be >= the divisor.
 
-This program uses an implementation of the "longSubtraction" program that's way the variable names are the ones of the "longSubtraction".
+Example:
+
+./longDivision 343456778384378290000000 3443499958888
 
 NOTE: the coutient will replace the dividend, so you may want to copy it to a different location.
 */
 
 char *longDivision(char *dividend, char divisor[]);
-int noArguments(int argc, char * argv[], char* version, char* author);
 
 int main(int argc, char* argv[])
 {
-	noArguments(argc, argv, "1.0", "Aisaac23");
-	char *result;
-	char *cuotient = calloc(strlen(argv[1])+1, sizeof(char));
-	strcpy(cuotient, argv[1]);
-	result = longDivision(cuotient, argv[2]);
+	if(argc < 3)
+	{
+		printf("Thre could be some data missing in: %s\n", argv[0]);
+		exit(EXIT_SUCCESS);
+	}
+
+	char *cuotient, *dividend;
+	dividend = calloc(strlen(argv[1])+1, sizeof(char));
+	strcpy(dividend, argv[1]);
+	cuotient = longDivision(dividend, argv[2]);
 	
-	printf("Reminder: %s, Cuotient: %s\n", result, cuotient);
+	printf("Reminder: %s, Cuotient: %s\n", dividend, cuotient);
 	
+	//...
+	free(dividend);
+	free(cuotient);
 	return EXIT_SUCCESS;
 }
 
-char *longDivision(char *minuend, char subtrahend[])
+char *longDivision(char *dividend, char divisor[])
 {
 	//With this one I reused the longSubtraction's function code and made some modifications.  
-	unsigned long long minuendSize = strlen(minuend), subtrahendSize = strlen(subtrahend), resultSize;
-	char *result, *absMinuend, *absSubtrahend, *cuotient, subChar[2];
+	unsigned long long dividendSize, divisorSize, CuotientIndex, divisorIndex, dividendIndex;
+	char *cuotient;
 	bool remainderZero = false;
-	int sub, loans;
+	int subtraction, loans;
 	
-	resultSize = (minuendSize >= subtrahendSize) ? minuendSize : subtrahendSize;
+	dividendSize = strlen(dividend);
+	divisorSize = strlen(divisor);
 	
-	result = calloc( resultSize+1, sizeof(char) );
-	absMinuend = calloc( resultSize+1, sizeof(char) );
-	absSubtrahend = calloc( resultSize+1, sizeof(char) );
-	cuotient = calloc( resultSize+1, sizeof(char) );//added
-	
-	for(long int i = 0; i<resultSize; i++)
-			cuotient[i] = result[i] = absMinuend[i] = absSubtrahend[i] = '0';
-		
-	memmove( absMinuend+(resultSize-minuendSize), minuend, minuendSize*sizeof(char) );
-	memmove( absSubtrahend+(resultSize-subtrahendSize), subtrahend, subtrahendSize*sizeof(char) );	
+	if( dividendSize < divisorSize )
+		return "0";
 
-	absMinuend[resultSize] = result[resultSize] = absSubtrahend[resultSize] = cuotient[resultSize] = '\0';
+	cuotient = calloc( dividendSize+1, sizeof(char) );
+	
+	for(long int i = 0; i<dividendSize; i++)
+			cuotient[i] = '0';
 
+	cuotient[dividendSize] = '\0';
+	
 	while(!remainderZero )
 	{		
 		//Subtraction loop, it's practically the same as in "LongSubtraction"
-		unsigned long long int subIndex = resultSize; // we subtract from left to right.
+		divisorIndex = divisorSize; // we subtract from left to right.
+		dividendIndex = dividendSize;
 		do
 		{
-			subIndex--;	
-			sub = (absMinuend[subIndex] - '0') - (absSubtrahend[subIndex] - '0');
-			if(sub < 0 && subIndex > 0)//case when we have, for example: 54678-9 
+			divisorIndex--;
+			dividendIndex--;
+			subtraction = (dividend[dividendIndex] - '0') - (divisor[divisorIndex] - '0');
+			if(subtraction < 0 && divisorIndex >= 0)//case when we have, for example: 54678-9 
 			{
-				sub +=10;
+				subtraction +=10;
 				
 				int next = 1;
-				loans = absMinuend[subIndex - next] - '0';//the one who loans 1
+				loans = dividend[dividendIndex - next] - '0';//the one who loans 1
 				
-				while(loans == 0)// when in minuend we have, for exmple: 50008 - 9
+				while(loans == 0)// when in dividend we have, for exmple: 50008 - 9
 				{
-					absMinuend[ subIndex - next ] = '9';
+					dividend[ dividendIndex - next ] = '9';
 					next++;
-					loans = absMinuend[ subIndex - next ] - '0';	
+					loans = dividend[ dividendIndex - next ] - '0';	
 				}
-				sprintf(subChar,"%u", loans-1);
-				absMinuend[ subIndex - next ] = subChar[0];
+				dividend[ dividendIndex - next ] = (char)( (loans-1) + '0' );
 			}
-			sprintf(subChar,"%u", sub);
-			result[ subIndex ] = subChar[0];
+			dividend[ dividendIndex ] = (char)( subtraction + '0' ) ;
 			
-		}while(subIndex > 0);//subIndex is unsigned so it'd cause a runtime error if it gets to -1
+		}while(divisorIndex > 0);//divisorIndex is unsigned so it'd cause a runtime error if it gets to -1
 
 		/*Here we check if we have a reminder of zero, in that case the we've finished long division*/
 		remainderZero = true;
 		unsigned long long zeros = 0;
-		for(unsigned long long index = 0; index<resultSize && remainderZero; index++)
-			if( result[index] != '0')
+		for(unsigned long long index = 0; index<dividendSize && remainderZero; index++)
+			if( dividend[index] != '0')
 				remainderZero = false;
 			else
 				zeros++;
 		/**************************************/
 		
-		/*Here we add one to the cuotient, since we've been able to subtract once the subtrahend from the minuend*/
-		unsigned long long int CuotientIndex = resultSize;
+		/*Here we add one to the cuotient, since we've been able to subtract once the divisor from the dividend*/
+		CuotientIndex = dividendSize;
 		bool added = false;
 		do
 		{
@@ -103,47 +110,33 @@ char *longDivision(char *minuend, char subtrahend[])
 
 		}while( CuotientIndex > 0 && !added );
 		/*************************************/	
-		//printf("%s\n", result);
-		//getchar();
-		strcpy(absMinuend, result);// we use the last result as our new minuend
-		absMinuend[resultSize] = '\0';
+		
 		//Here we check whether the dividend has became a number lower than de divisor
-		int subI = 0;
-		if( resultSize - (zeros) == subtrahendSize )
-		{
-			for( zeros=zeros ; zeros < resultSize; zeros++)
-				if( (absMinuend[zeros]-'0') - (subtrahend[subI++]-'0') <= 0 )
-					remainderZero = ( ( (absMinuend[zeros]-'0') - (subtrahend[subI++]-'0') ) < 0 ) ? true: false;
-				else
-					zeros = resultSize;
-		}
-  		else if( resultSize - (zeros) < subtrahendSize )
+		int sorIndex = 0, sub;
+		
+		if( dividendSize - (zeros) < divisorSize )
 			remainderZero = true;
+		else if( dividendSize - (zeros) == divisorSize )
+		{
+			for( int endIndex=zeros ; endIndex < dividendSize; endIndex++)
+			{
+				sub = (dividend[endIndex]-'0') - (divisor[sorIndex++]-'0');
+				if( sub <= 0 )
+					remainderZero = sub < 0 ? true: false;
+				else
+					zeros = dividendSize;
+			}
+			
+		}
+  		else
+			remainderZero = false;
 	}
 	
 	//most of the times you'll have spare zeros in the left, so we rotate 
-	while(result[0] == '0' && strlen(result) > 1)
-		memmove(result, result+1, resultSize*sizeof(char) );
-	while(cuotient[0] == '0' && strlen(result) > 1)
-		memmove(cuotient, cuotient+1, resultSize*sizeof(char) );
-	
-	strcpy(minuend, cuotient);// so the value pointed by the minuend is now our cuotient
-	
-	free(absSubtrahend);
-	free(absMinuend);
-	free(cuotient);
+	while(dividend[0] == '0' && strlen(dividend) > 1)
+		memmove(dividend, dividend+1, dividendSize*sizeof(char)+1 );
+	while(cuotient[0] == '0' && strlen(cuotient) > 1)
+		memmove(cuotient, cuotient+1, dividendSize*sizeof(char)+1 );
 
-	return result;
-}
-
-int noArguments(int argc, char * argv[], char* version, char* author)
-{
-	if(argc <= 1)
-	{
-		
-		printf("\n%s ver %s by %s\n", argv[0], version, author);
-		exit(EXIT_SUCCESS);
-	}
-	else
-		return 1;
+	return cuotient;
 }
